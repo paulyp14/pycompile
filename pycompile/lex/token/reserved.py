905f1209ad5c4b1f97b73991186d50721a240152
Paul Piggott <Flying-piggott@hotmail.com>
2021-02-12 17:36:56 -0500

    added negative lookahead to reserved keyword regex to allow for identifiers that start the same as a reserved keyword (like returning, classy, etc.)

--- pycompile/lex/token/reserved.py
+++ pycompile/lex/token/reserved.py
@@ -5,7 +5,9 @@ from pycompile.lex.token.token import Token
 
 
 class Reserved(Token):
-    pattern: Pattern = re.compile('^(if|then|else|integer|float|string|void|public|private|func|var|class|while|read|write|return|main|inherits|break|continue)')
+    # negative lookahead is necessary here because any reserved keyword that is followed by another valid character
+    # becomes an identifier
+    pattern: Pattern = re.compile('^(?!(if|then|else|integer|float|string|void|public|private|func|var|class|while|read|write|return|main|inherits|break|continue)[a-zA-Z_])(if|then|else|integer|float|string|void|public|private|func|var|class|while|read|write|return|main|inherits|break|continue)')
 
     def __init__(self, code: str):
         # reserved keyword is both lexeme and name
